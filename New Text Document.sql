42cf12464db2b7abeea739aa5ff996c95fd71d5d
AlexTicleanu <48204339+AlexTicleanu@users.noreply.github.com>
2019-03-14 12:04:27 +0200

    Add files via upload

---
+++ New Text Document.sql
@@ -0,0 +1,39 @@
+DELIMITER GO
+
+CREATE EVENT automatic_clean_up
+    ON SCHEDULE 
+	EVERY 1 DAY
+   ON COMPLETION PRESERVE
+	DISABLE
+	COMMENT 'Clears out table each 10 seconds.'
+	DO BEGIN
+
+   SET foreign_key_checks = 0;
+
+
+SET @ref = (SELECT fod.id FROM emag_scm_dante.forecast_order_decisions fod WHERE fod.created  curdate()-3 ORDER BY fod.id DESC LIMIT 1);
+ 
+
+DELETE dpp FROM emag_scm_dante.automatic_supply_decisions_product_performance dpp
+JOIN (
+    SELECT fod.id
+    FROM forecast_order_decisions fod
+    INNER JOIN automatic_supply_decisions_product_performance dpp on fod.id = dpp.`forecast_order_decision_id`
+    WHERE fod.id  @ref
+    AND fod.status = 'deleted' 
+    ORDER BY fod.id
+    LIMIT 100000
+) sel ON dpp.forecast_order_decision_id = sel.id;
+
+
+DELETE FROM forecast_order_decisions
+WHERE 
+status = 'deleted'
+AND id  @ref
+ORDER BY id ASC
+LIMIT 100000;
+
+      
+SET foreign_key_checks = 1;
+END; 
+GO  
