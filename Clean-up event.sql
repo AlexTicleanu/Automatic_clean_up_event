58c1a2dd541ce1d7dd92c0472fae35245ff35278
AlexTicleanu <48204339+AlexTicleanu@users.noreply.github.com>
2019-03-14 14:07:18 +0200

    Update Clean-up event.sql

--- Clean-up event.sql
+++ Clean-up event.sql
@@ -1,11 +1,11 @@
 DELIMITER GO
 
 CREATE TABLE IF NOT EXISTS event_log(
-	'id' int(11) NOT NULL AUTO_INCREMENT,
-	'event_name' VARCHAR(128) NOT NULL,
-	'count_decisions' int(11) DEFAULT NULL, 
-	'count_p_performance' int(11) DEFAULT NULL, 
-	'start/end' TIMESTAMP NULL DEFAULT NULL,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`event_name` VARCHAR(128) NOT NULL,
+	`count_decisions` int(11) DEFAULT NULL, 
+	`count_p_performance` int(11) DEFAULT NULL, 
+	`start/end` TIMESTAMP NULL DEFAULT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -20,33 +20,39 @@ CREATE EVENT automatic_clean_up
 
 SET foreign_key_checks = 0;
 
-INSERT INTO event_log(event_name,count_decisions,count_p_performance,start/end)
-VALUES (automatic_clean_up,(SELECT COUNT(id) from forecast_order_decisions),(SELECT COUNT(id) from automatic_supply_decisions_product_performance),(SELECT CURDATE()));
+SET @fod_before = (SELECT COUNT(id) from forecast_order_decisions);
+SET @aspp_before = (SELECT COUNT(id) from automatic_supply_decisions_product_performance);
 
-SET @ref = (SELECT fod.id FROM emag_scm_dante.forecast_order_decisions fod WHERE fod.created  curdate()-3 ORDER BY fod.id DESC LIMIT 1);
+INSERT INTO event_log(`event_name`,`count_decisions`,`count_p_performance`,`start/end`)
+VALUES ('automatic_clean_up',@fod_before,@aspp_before,(SELECT CURDATE()));
+
+SET @ref = (SELECT fod.id FROM forecast_order_decisions fod WHERE fod.created <= curdate()-3 ORDER BY fod.id DESC LIMIT 1);
  
 
-DELETE dpp FROM emag_scm_dante.automatic_supply_decisions_product_performance dpp
+DELETE dpp FROM automatic_supply_decisions_product_performance dpp
 JOIN (
     SELECT fod.id
     FROM forecast_order_decisions fod
     INNER JOIN automatic_supply_decisions_product_performance dpp on fod.id = dpp.`forecast_order_decision_id`
-    WHERE fod.id  @ref
+    WHERE fod.id <= @ref
     AND fod.status = 'deleted' 
     ORDER BY fod.id
-    LIMIT 100000
 ) sel ON dpp.forecast_order_decision_id = sel.id;
 
 
 DELETE FROM forecast_order_decisions
 WHERE 
 status = 'deleted'
-AND id  @ref
-ORDER BY id ASC
-LIMIT 100000;
+AND id <= @ref
+ORDER BY id ASC;
 
-INSERT INTO event_log(event_name,count_decisions,count_p_performance,start/end)
-VALUES (automatic_clean_up,(SELECT COUNT(id) from forecast_order_decisions),(SELECT COUNT(id) from automatic_supply_decisions_product_performance),(SELECT CURDATE()));																		    
+
+
+SET@fod_after = (SELECT COUNT(id) from forecast_order_decisions);
+SET@aspp_after = (SELECT COUNT(id) from automatic_supply_decisions_product_performance);
+
+INSERT INTO event_log(`event_name`,`count_decisions`,`count_p_performance`,`start/end`)
+VALUES ('automatic_clean_up',@fod_after,@aspp_after,(SELECT CURDATE()));																		    
       
 SET foreign_key_checks = 1;
 END; 
