8e3b115decd96fe7a432bce329437575fc6828c8
AlexTicleanu <48204339+AlexTicleanu@users.noreply.github.com>
2019-04-16 14:26:24 +0300

    Update Clean-up event.sql

--- Clean-up event.sql
+++ Clean-up event.sql
@@ -1,95 +1,5 @@
 DELIMITER //
 
-#CREATE A TABLE FOR THE EVENT TO LOG
-CREATE TABLE IF NOT EXISTS event_log(
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`event_name` VARCHAR(128) NOT NULL,
-	`state` ENUM('start','middle','stop','successful','error') NOT NULL,
-	`count_decisions` int(11) DEFAULT NULL, 
-	`count_p_performance` int(11) DEFAULT NULL, 
-	`start/end` TIMESTAMP NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-);
-
-
-
-#CREATE PROCEDURE FOD
-DROP PROCEDURE IF EXISTS schedule_delete_fod;
-CREATE PROCEDURE schedule_delete_fod(IN REF INT)
-BEGIN 
-
-SET @ct = (select MIN(id) from forecast_order_decisions where `status` = 'deleted'); 
-	WHILE (@ct+1) < REF   
-		DO 
-		DELETE FROM forecast_order_decisions
-		WHERE 
-		status = 'deleted'
-		AND id < REF
-		ORDER BY id ASC 
-		LIMIT 10000;
-		SET @ct = (select MIN(id) from forecast_order_decisions where `status` = 'deleted');
-	END WHILE;
-	INSERT INTO event_log(`event_name`,`state`,`count_decisions`,`count_p_performance`,`start/end`) 
-	values ('automatic_clean_up','stop',(SELECT COUNT(id) from forecast_order_decisions),(SELECT COUNT(id) from automatic_supply_decisions_product_performance),(SELECT NOW()));
-END// 
-
-#CREATE PROCEDURE DPP
-
-DROP PROCEDURE IF EXISTS schedule_delete_dpp;
-CREATE PROCEDURE schedule_delete_dpp(IN REF INT)
-BEGIN
-
-SET @dt = (SELECT COUNT(dpp.id) FROM automatic_supply_decisions_product_performance dpp
-		INNER JOIN forecast_order_decisions fod on dpp.forecast_order_decision_id = fod.id
-		WHERE date(fod.created) <= date(curdate()-5)
-		AND fod.status = 'deleted');
-SET @INO = 0; 
-
-	WHILE @dt - @INO > 0 
-			DO 
-			DELETE dpp FROM automatic_supply_decisions_product_performance dpp
-			JOIN 
-    		(SELECT fod.id
-    		FROM forecast_order_decisions fod
-    		INNER JOIN automatic_supply_decisions_product_performance dpp on fod.id = dpp.`forecast_order_decision_id`
-    		WHERE fod.id <= REF
-    		AND fod.status = 'deleted'
-			LIMIT 10000)
-			sel ON dpp.forecast_order_decision_id = sel.id;
-			SET @INO = @INO + 10000;
-			
-	END WHILE;
-	
-	INSERT INTO event_log(`event_name`,`state`,`count_decisions`,`count_p_performance`,`start/end`)
-	VALUES ('automatic_clean_up','middle',NULL,(SELECT COUNT(id) from automatic_supply_decisions_product_performance),(SELECT NOW()));
-
-END
-//
-
-DROP PROCEDURE IF EXISTS dppfod_safe_net;
-CREATE PROCEDURE dppfod_safe_net()
-BEGIN
-	IF (SELECT IF (EXISTS (SELECT id from automatic_supply_decisions_product_performance LIMIT 1), 1 , 0) = 1 )
-		 THEN 
-		 	INSERT INTO event_log (`event_name`,`state`) 
-			VALUES ('process successfully done','successful'); 
-	ELSE
-		INSERT INTO event_log (`event_name`) 
-		VALUES ('ERROR:EVENT DELETED ALL DPP','error');
-	END IF; 
-
-	IF (SELECT IF (EXISTS (SELECT id from forecast_order_decisions LIMIT 1), 1 , 0) > 0)
-		 THEN 
-		 	INSERT INTO event_log (`event_name`,`state`) 
-			VALUES ('process successfully done','successful'); 
-	ELSE
-		INSERT INTO event_log (`event_name`) 
-		VALUES ('ERROR:EVENT DELETED ALL FOD','error');
-	END IF; 
-       
-END
-//
-
 #CREATE THE EVENT
 CREATE EVENT automatic_clean_up
     ON SCHEDULE 
@@ -101,11 +11,11 @@ CREATE EVENT automatic_clean_up
 
 SET foreign_key_checks = 0;
 
-
+DROP TABLE IF EXISTS forecast_order_decisions_copy ; 
 CREATE TABLE IF NOT EXISTS forecast_order_decisions_copy LIKE forecast_order_decisions ;
 INSERT INTO forecast_order_decisions_copy (`id`, `forecast_rule_id`, `product_id`, `daily_average`, `last_acquisition_price`, `stock`, `stock_target`, `stock_min`, `stock_max`, `pm_ordered_quantity`, `unconfirmed_quantity`, `reserved_quantity`, `resulted_quantity`, `initial_resulted_quantity`, `supplier_id`, `price`, `price_in_supplier_currency`, `currency_id`, `supplier_order_line_id`, `message`, `error_id`, `status`, `reject_reason_id`, `user_id`, `stock_target_med_resulted_qty`, `created`, `modified`) SELECT `id`, `forecast_rule_id`, `product_id`, `daily_average`, `last_acquisition_price`, `stock`, `stock_target`, `stock_min`, `stock_max`, `pm_ordered_quantity`, `unconfirmed_quantity`, `reserved_quantity`, `resulted_quantity`, `initial_resulted_quantity`, `supplier_id`, `price`, `price_in_supplier_currency`, `currency_id`, `supplier_order_line_id`, `message`, `error_id`, `status`, `reject_reason_id`, `user_id`, `stock_target_med_resulted_qty`, `created`, `modified` FROM `forecast_order_decisions`;
 
-
+DROP TABLE IF EXISTS automatic_supply_decisions_product_performance_copy; 
 CREATE TABLE IF NOT EXISTS automatic_supply_decisions_product_performance_copy LIKE automatic_supply_decisions_product_performance;
 INSERT INTO automatic_supply_decisions_product_performance_copy (`id`, `forecast_order_decision_id`, `country_id`, `product_performance_id`) SELECT `id`, `forecast_order_decision_id`, `country_id`, `product_performance_id` FROM `automatic_supply_decisions_product_performance`;
 
